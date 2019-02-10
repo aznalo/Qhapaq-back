ee911646810973f5f8cc857c0122358a441a485a
Meltens <irimamekun@gmail.com>
2019-02-10 21:35:45 +0900

    コンフリクト解消

--- models.rb
+++ models.rb
@@ -1,15 +1,9 @@
 require 'bundler/setup'
 Bundler.require
-<<<<<<< HEAD
 require './controllers/user'
 require './controllers/genre'
 require './controllers/category'
 require './controllers/menu'
-=======
-require 'net/http'
-require 'uri'
-require 'json'
->>>>>>> eb1932f1ef684c29917e8ceca3c4a40682e250ba
 
 config = YAML.load_file('./database.yml')
 
@@ -27,7 +21,6 @@ after do
   ActiveRecord::Base.connection.close
 end
 
-<<<<<<< HEAD
 class User < ActiveRecord::Base
   has_secure_password
   has_many :user_tokens, dependent: :destroy
@@ -45,29 +38,11 @@ class Menu < ActiveRecord::Base
   belongs_to :genre
   belongs_to :category
   has_many :ingredients, dependent: :destroy
-=======
-class Genre < ActiveRecord::Base
-  has_many :categories
-end
-
-class Category < ActiveRecord::Base
-  belongs_to :genre
-  has_many :menus
-end
-
-class Menu < ActiveRecord::Base
-  belongs_to :category
-  has_many :ingredient
->>>>>>> eb1932f1ef684c29917e8ceca3c4a40682e250ba
 end
 
 class Ingredient < ActiveRecord::Base
   belongs_to :menu
 end
-<<<<<<< HEAD
-
 class Category < ActiveRecord::Base
   has_many :menus, dependent: :destroy
 end
-=======
->>>>>>> eb1932f1ef684c29917e8ceca3c4a40682e250ba
