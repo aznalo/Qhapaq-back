34b0efc95fedb7e8ef9a7221636fb294dec98083
Meltens <irimamekun@gmail.com>
2019-02-10 11:28:59 +0900

    I executed a command to 'rake db:migrate'

--- db/migrate/20190210021303_create_categories.rb
+++ db/migrate/20190210021303_create_categories.rb
@@ -1,4 +1,10 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
+    create_table :categories do |t|
+      t.integer :genre_id, null: false
+      t.string  :name,     null: false
+      t.string  :description
+      t.timestamps         null: false
+    end
   end
 end
--- db/migrate/20190210021320_create_menus.rb
+++ db/migrate/20190210021320_create_menus.rb
@@ -1,4 +1,10 @@
 class CreateMenus < ActiveRecord::Migration[5.2]
   def change
+    create_table :menus do |t|
+      t.integer :category_id, null: false
+      t.string  :name,        null: false
+      t.string  :description
+      t.timestamps            null: false
+    end
   end
 end
