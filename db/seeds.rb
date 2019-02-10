0cf4b6ee5e4c455a09bcad8537c4ecb6d684cae6
Meltens <irimamekun@gmail.com>
2019-02-10 19:56:33 +0900

    カテゴリーとジャンルのseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,30 @@
-['主食', '副菜', '汁物', '甘味'].each do |t|
+genres = ['主食', '副菜', '汁物', '甘味']
+genres.each do |t|
   Genre.create(name: t)
+end
+
+main_categories = ['和食', '中華', 'フレンチ', 'イタリアン', 'トルコ', 'その他']
+
+categories.each do |t|
+  Category.create(name: t)
+end
+
+genres.each do |t|
+  categories.each do |tt|
+    GenreCategory.create(
+      genre: Genre.find_by(name: t),
+      category: Category.find_by(name: tt)
+    )
+  end
+end
+
+[ 
+  {name: '鯖の味噌煮', category: '和食'},
+  {name: '回鍋肉', category: '中華'}, 
+  {name: '白身魚のナージュ', category: 'フレンチ'}
+].each do |t|
+  Menu.create(
+    name: t[:name],
+    category: Category.find_by(name: t[:category])
+  )
 end
