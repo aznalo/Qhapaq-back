2f607c9a6cb866f77f1680b9e05a3d9519fa61ca
Gin.Furuya <irimamekun@gmail.com>
2019-02-11 19:56:24 +0900

    Menu関連の更新 (#4)
    * 初期ファイルを作成
    
    * bundle install
    
    * データベースのファイル作成
    
    * I executed a command to 'rake db:migrate'
    
    * モデル間の関連付け
    
    * create control files
    
    * UserModelを作成
    
    * genreのcontroller作成
    
    * Userの認証系統の実装
    
    * 認証成功時にタイムゾーンが設定されないバグ修正
    
    * create seeds.rb
    
    * add get categorys
    
    * ジャンルとカテゴリの間に中間テーブルを置いて多対多の関係を作る
    
    * カテゴリーとジャンルのseed作成
    
    * カテゴリの関連付けを変更
    
    * メニュー一覧を取得できるように
    
    * コンフリクト解消
    
    * post menu
    
    * 材料に単位の項目追加
    
    * 単位をint
    
    * seedsにadministratorを追加
    
    * paramsの取り出し名を変更
    
    * menuのアップデートに関する変更

--- controllers/menu.rb
+++ controllers/menu.rb
@@ -1,9 +1,51 @@
+# index
 get '/menu/:id' do
   menu = Menu.find_by(id: params[:id])
+  menu.attributes.merge({ingredients: menu.ingredients}).to_json
+end
+
+# create
+post '/menu' do
+  menu_params = JSON.parse(request.body.read)
+  menu = Menu.new(hoge(menu_params))
+  status 500 unless menu.save
+  menu_params['ingredients'].each { |v| fuga(menu, v) }
+  menu.to_json
+end
+
+# update
+post '/menu/:id' do
+  menu_params = JSON.parse(request.body.read)
+  menu = Menu.find_by(id: params[:id])
+  status 404 unless menu
+  status 500 unless menu.update(hoge(menu_params))
+  menu_params['ingredients'].each { |v| fuga(menu, v) }
+  menu.to_json
+end
+
+private
+
+def hoge(params)
   {
-    menu: menu,
-    ingredients: menu.ingredients
-  }.to_json
+    genre: Genre.find_by(id: params['genre_id']),
+    category_id: 1,
+    name: params['name']
+  }
+end
+
+def fuga(menu, params)
+  ingredient = Ingredient.find_or_initialize_by(
+    name: params['name'],
+    menu: menu
+  )
+  ingredient.update_attributes(
+    menu:        menu,
+    name:        params['name'],
+    amount:      params['amount'],
+    unit:        params['unit'],
+    cost:        params['cost'],
+    description: params['description']
+  )
 end
 
 post '/menu' do
