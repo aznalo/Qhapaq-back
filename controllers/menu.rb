dc9c20dce3e3340a7966db8791355491c48e80ef
Gin.Furuya <irimamekun@gmail.com>
2019-02-13 12:39:06 +0900

    メニューの材料の編集機能の追加 (#5)
    * 初期ファイルを作成
    
    * bundle install
    
    * データベースのファイル作成
    
    * I executed a command to 'rake db:migrate'
    
    * モデル間の関連付け
    
    * create control files
    
    * UserModelを作成
    
    * genreのcontroller作成
    
    * Userの認証系統の実装
    
    * 認証成功時にタイムゾーンが設定されないバグ修正
    
    * create seeds.rb
    
    * add get categorys
    
    * ジャンルとカテゴリの間に中間テーブルを置いて多対多の関係を作る
    
    * カテゴリーとジャンルのseed作成
    
    * カテゴリの関連付けを変更
    
    * メニュー一覧を取得できるように
    
    * コンフリクト解消
    
    * post menu
    
    * 材料に単位の項目追加
    
    * 単位をint
    
    * seedsにadministratorを追加
    
    * paramsの取り出し名を変更
    
    * menuのアップデートに関する変更

--- controllers/menu.rb
+++ controllers/menu.rb
@@ -47,28 +47,3 @@ def fuga(menu, params)
     description: params['description']
   )
 end
-
-post '/menu' do
-  menu_params = JSON.parse(request.body.read)
-  menu = Menu.new({
-    genre: Genre.find_by(id: menu_params['genre_id']),
-    category_id: 1,
-    name: menu_params['name']
-  })
-  if menu.save
-    menu_params['ingredients'].each do |i|
-      Ingredient.create(
-        menu: menu,
-        name: i['name'],
-        amount: i['amount'],
-        unit: i['unit'],
-        cost: i['cost'],
-        description: i['description']
-      )
-    end
-    menu.to_json
-  else
-    status 500
-  end
-
-end
